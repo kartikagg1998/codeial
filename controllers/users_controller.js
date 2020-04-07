93d7fbf136346258c41d3911608d2cd34f6ffeb4
Shubham <shubhamrocks110@gmail.com>
2020-04-07 12:43:33 +0530

    add acontoller and routers for users

---
+++ controllers/users_controller.js
@@ -0,0 +1,4 @@
+module.exports.profile=function(request,response)
+ {
+    response.end('<h1>Users profile page</h1>');
+ }
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express=require('express');
 
 const router=express.Router();
 const homeController=require('../controllers/home_controller');
-//const profileController=require('../controllers/home_controller');
+
 
 router.get('/',homeController.home);  //home is a function which is in contollers(module.export.home.............)
-//router.get('/profile',profileController.profile); 
-console.log("router loaded");
+router.use('/users',require('./users'));
+
+//for any further routes access from here
+//router.use('/routermame',require('./routerfile))
 
 module.exports=router;//use export router so that it is available to main index.js file
