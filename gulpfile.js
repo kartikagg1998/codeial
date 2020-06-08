904421e038b25e19d35e9a5613c76cf83156e180
Kartik Aggarwal <54892805+kartikagg1998@users.noreply.github.com>
2020-06-08 17:03:10 +0530

    done js and images minification and create helper function for assets

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ const gulp=require('gulp');
  const sass=require('gulp-sass');//gulpsass library converts sass ino css
  const cssnano=require('gulp-cssnano');//cssnano converts css into oneline
  const rev=require('gulp-rev');//rename the css file and add hash symbol with it
+ const uglify = require('gulp-uglify-es').default;//for js
+ const imagemin = require('gulp-imagemin');//for images
+ const del = require('del');
+
 
  
 
@@ -27,6 +31,46 @@ const gulp=require('gulp');
              merge:true
          }))
       .pipe(gulp.dest('./public/assets'));
-       done();  //gulp css command create problem so changr again path in env naraiable remove public
+      done();  //gulp css command create problem so changr again path in env naraiable remove public
 
- })
+ });
+
+ gulp.task('js', function(done){
+  console.log('minifying js...');
+   gulp.src('./assets/**/*.js')
+  .pipe(uglify())
+  .pipe(rev())
+  .pipe(gulp.dest('./public/assets'))
+  .pipe(rev.manifest({
+      cwd: 'public',
+      merge: true
+  }))
+  .pipe(gulp.dest('./public/assets'));
+  done()
+});
+
+gulp.task('images', function(done){
+  console.log('compressing images...');
+  gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+  .pipe(imagemin())
+  .pipe(rev())
+  .pipe(gulp.dest('./public/assets'))
+  .pipe(rev.manifest({
+      cwd: 'public',
+      merge: true
+  }))
+  .pipe(gulp.dest('./public/assets'));
+  done();
+});
+
+
+// empty the public/assets directory
+gulp.task('clean:assets', function(done){
+  del.sync('./public/assets');
+  done();
+});
+
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function(done){
+  console.log('Building assets');
+  done();
+});
